e06ab3addbf680f51a1e22f7ad0e22d4f7f1e645
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2022-04-23 20:56:04 +0200

    Header commit 💎🐔🥚🤣

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div className="hero">
-      <h1>Home</h1>
+      <div className="header">
+        <h1>Discover rare digital art and collection NFTs</h1>
+        <h5>
+          Digital marketplace for crypto collectibles and non-fungible tokens
+          [NFTs]. Buy, Sell and Discover exclusive digital assets.
+        </h5>
+      </div>
     </div>
   );
 };
